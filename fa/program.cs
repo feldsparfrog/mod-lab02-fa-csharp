980c0f3b506e683f78704ece41bd79add7426047
feldsparfrog <schepalovnickolay@gmail.com>
2026-05-25 07:25:22 +0300

    Update program.cs

--- fa/program.cs
+++ fa/program.cs
@@ -1,10 +1,9 @@
-using NET;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NET;
 namespace fans
 {
   public class State
